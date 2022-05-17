db723e76852782accd62fd1c69865cd20223cc45
hieunghia-pat <19520178@gm.uit.edu.vn>
2022-05-17 14:22:34 +0700

    created login, admin and annotator components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './login/login.component';
+import { AdminComponent } from './admin/admin.component';
+import { AnnotatorComponent } from './annotator/annotator.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    AdminComponent,
+    AnnotatorComponent
   ],
   imports: [
     BrowserModule,
